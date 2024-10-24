efa0a597e4f82ea7b1f76a821cbde7dcdfc6ef0a
David <dmg_92@hotmail.com>
2024-10-24 15:52:52 -0600

    optimizador cart v1

--- src/components/redux/cart/cartReducer.js
+++ src/components/redux/cart/cartReducer.js
@@ -7,27 +7,53 @@ const initialState = {
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART: {
-      const { id } = action.payload; // Extraer cantidad del payload
+      const { id, quantity, prices } = action.payload;
       const existingProductIndex = state.cart.findIndex(item => item.id === id);
-
+      console.log(existingProductIndex)
+    
       if (existingProductIndex !== -1) {
-        // Si el producto ya existe, actualizar la cantidad sumando la del item existente
-        const updatedCart = state.cart.map((item, index) => 
-          index === existingProductIndex
-            ? { ...item, quantity: item.quantity + 1 } // Aumentar la cantidad desde el item
-            : item
-        );
-
+        // Si el producto ya existe, actualizar la cantidad y recalcular el precio
+        const updatedCart = state.cart.map((item, index) => {
+          if (index === existingProductIndex) {
+            const updatedQuantity = item.quantity + 1;
+            
+            var product_price = updatedQuantity >= item.prices.min_wholesale_quantity 
+              ? item.prices.wholesale_sale_price 
+              : item.prices.unit_sale_price;
+            
+            if (!product_price){
+              product_price = item.prices.unit_sale_price
+            }
+              
+              console.log('product_price', product_price)
+            return { 
+              ...item, 
+              quantity: updatedQuantity, 
+              product_price 
+            };
+          }
+    
+          return item; // Devolver el producto sin modificar
+        });
+    
         return {
           ...state,
           cart: updatedCart,
         };
       }
+    
+      // Si el producto no existe, agregarlo con la cantidad y precio correctos
+      var product_price = quantity >= prices.min_wholesale_quantity 
+        ? prices.wholesale_sale_price 
+        : prices.unit_sale_price;
 
-      // Agregar producto nuevo al carrito con la cantidad que viene del payload
+        if (!product_price){
+          product_price = prices.unit_sale_price
+        }
+
       return {
         ...state,
-        cart: [...state.cart, { ...action.payload }], // No establecer cantidad aquí, ya que viene del payload
+        cart: [...state.cart, { ...action.payload, product_price }],
       };
     }
 
