320f9073b89229dc5311758c00914e8b1342e1bd
David <dmg_92@hotmail.com>
2024-11-06 21:51:08 -0600

    login admin

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -71,7 +71,7 @@ function Login({ onLogin }) {
     if (response.store) {
       navigate("/nueva-venta/");
     } else {
-      navigate("/aun-no-existe/");
+      navigate("/productos/");
     }
   };
 
