9a76c40b242ac7b4963e49774d13b7b93e330b63
David <dmg_92@hotmail.com>
2025-01-22 18:12:18 -0600

    utils

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,37 +1,33 @@
 import * as XLSX from "xlsx";
 
-export const getToday = () =>
-    {
-      const today = new Date();
-      const year = today.getFullYear();
-      const month = String(today.getMonth() + 1).padStart(2, "0"); // Add 1 because months are 0-indexed
-      const day = String(today.getDate()).padStart(2, "0");
-      return `${year}-${month}-${day}`; // Format as 'YYYY-MM-DD
-    }
-
-
-export const getFormattedDate = (date = new Date()) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
-    const day = String(date.getDate()).padStart(2, '0');
-    
-    return `${year}/${month}/${day}`;
+export const getToday = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Add 1 because months are 0-indexed
+  const day = String(today.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`; // Format as 'YYYY-MM-DD
 };
 
+export const getFormattedDate = (date = new Date()) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day = String(date.getDate()).padStart(2, "0");
 
-export const exportToExcel = (data, prefix_name, use_today=true) => {  
-    console.log(data, prefix_name)
-    const worksheet = XLSX.utils.json_to_sheet(data, prefix_name);
+  return `${year}/${month}/${day}`;
+};
 
-    // Crear un libro de trabajo
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Reporte");
-    const formattedDate = getFormattedDate()
+export const exportToExcel = (data, prefix_name, use_today = true) => {
+  console.log(data, prefix_name);
+  const worksheet = XLSX.utils.json_to_sheet(data, prefix_name);
 
-    if (use_today){
-        XLSX.writeFile(workbook, `${prefix_name} ${formattedDate}.xlsx`);
-    }
-    else{
-        XLSX.writeFile(workbook, `${prefix_name}.xlsx`);
-    }
-  };
+  // Crear un libro de trabajo
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Reporte");
+  const formattedDate = getFormattedDate();
+
+  if (use_today) {
+    XLSX.writeFile(workbook, `${prefix_name} ${formattedDate}.xlsx`);
+  } else {
+    XLSX.writeFile(workbook, `${prefix_name}.xlsx`);
+  }
+};
