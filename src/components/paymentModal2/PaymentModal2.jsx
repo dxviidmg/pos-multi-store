95254a1bc8b4635dd7aebdd5df9f926e3c984180
David <dmg_92@hotmail.com>
2025-05-07 15:32:43 -0600

    v1

--- src/components/paymentModal2/PaymentModal2.jsx
+++ src/components/paymentModal2/PaymentModal2.jsx
@@ -1,26 +1,10 @@
-import React, { useMemo, useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import CustomModal from "../commons/customModal/customModal";
 import { Col, Form, Row } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import CustomButton from "../commons/customButton/CustomButton";
-import { cleanCart, removeClientfromCart } from "../redux/cart/cartActions";
-import { createSale } from "../apis/sales";
-import { hidePaymentModal } from "../redux/paymentModal/PaymentModalActions";
-import Swal from "sweetalert2";
-import { getUserData } from "../apis/utils";
-import { handlePrintTicket } from "../utils/utils";
-
-function roundUpCustom(value) {
-  const intPart = Math.floor(value); // Parte entera
-  const decimalPart = value - intPart; // Parte decimal
-
-  if (decimalPart === 0) return value; // Si es entero, se queda igual
-  if (decimalPart <= 0.5) return intPart + 0.5; // Si es hasta 0.5, sube a 0.5
-  return Math.ceil(value); // Si es mayor a 0.5, sube al siguiente entero
-}
 
 const INITIAL_PAYMENT_STATE = { paidWith: 0, change: 0 };
-const INITIAL_SALE_EXCHANGE_STATE = { refunded: 0, payment: 0 };
 
 const PaymentModal2 = () => {
   const inputPaymentRef = useRef(null);
@@ -28,73 +12,46 @@ const PaymentModal2 = () => {
     (state) => state.PaymentModal2Reducer
   );
 
-  console.log("showPaymentModal22", showPaymentReservationModal, reservation);
-  //  const sale = useSelector((state) => state.saleReduce.sale);
-  const movementType = useSelector((state) => state.cartReducer.movementType);
-
   const [action, setAction] = useState("Liquidar");
 
   const [payment, setPayment] = useState(INITIAL_PAYMENT_STATE);
   const [referencePayment, setReferencePayment] = useState("");
-  const [saleExchange, setSaleExchange] = useState(INITIAL_SALE_EXCHANGE_STATE);
 
   const [paymentMethod, setPaymentMethod] = useState("EF");
-  const urlPrinter = getUserData().store_url_printer;
-
-  const dispatch = useDispatch();
 
   useEffect(() => {
     if (showPaymentReservationModal) {
       setTimeout(() => {
         inputPaymentRef.current?.focus();
-      }, 100); // Pequeño retraso para permitir el renderizado
+      }, 100);
     }
   }, [showPaymentReservationModal]);
 
-  const { total, totalDiscount } = useMemo(() => {
-    const total = 100;
+  const handleCreatePayment = async (printTicket = false) => {
+    const data = {
+      sale: reservation.id,
+      payment: payment.paidWith - payment.change,
+      paymentMethod,
+      action
+    }
 
-    const totalDiscount = 100;
-
-    return { total, totalDiscount };
-  }, [reservation]);
-
-  useEffect(() => {
-    const handleShortcut = (event) => {
-      if (event.ctrlKey && event.key === "f") {
-        event.preventDefault();
-        handleCreateSale();
-      } else if (event.ctrlKey && event.key === "g") {
-        event.preventDefault();
-        handleCreateSale(true);
-      }
-    };
-
-    window.addEventListener("keydown", handleShortcut);
-    return () => window.removeEventListener("keydown", handleShortcut);
-  }, [totalDiscount, paymentMethod, reservation, payment]);
-
-  const handleChangePayments = (e) => {
-    const { name, value } = e.target;
+    console.log(data)
   };
-
-  const totalPaymentInput = (0 * 100) / 100;
-
-  const handleCreateSale = async (printTicket = false) => {};
 
   const handlePaidWithChange = async (e) => {
     let value = Number(e.target.value);
-    console.log("** handlePaidWithChange");
-    console.log("***", movementType);
     setPayment({
       paidWith: value,
-      change: value + reservation.paid - reservation.total
+      change: value + reservation.paid - reservation.total,
     });
   };
 
   const handleDisableButton = () => {
-    if (movementType === "apartado") {
-      return false;
+    if (payment.paidWith === 0) return true;
+    if (payment.paidWith >= (reservation.total - reservation.paid)) return true;
+    if (action === 'Liquidar') {
+      console.log()
+      return payment.paidWith <= (reservation.total - reservation.paid);
     }
     return false;
   };
@@ -160,16 +117,19 @@ const PaymentModal2 = () => {
       <div className="custom-section">
         <h2>Totales</h2>
         <Row>
-        <Col md={3}>
+          <Col md={3}>
             <Form.Label>Pagado</Form.Label>
             <Form.Control type="number" value={reservation.paid} disabled />
           </Col>
 
           <Col md={3}>
             <Form.Label>Deuda</Form.Label>
-            <Form.Control type="number" value={reservation.total - reservation.paid} disabled />
+            <Form.Control
+              type="number"
+              value={reservation.total - reservation.paid}
+              disabled
+            />
           </Col>
-
 
           <Col md={3}>
             <Form.Label>Pago con</Form.Label>
@@ -203,15 +163,12 @@ const PaymentModal2 = () => {
       <div className="custom-section">
         <Row>
           <Col md={6}>
-
-
             <CustomButton
               disabled={handleDisableButton()}
               fullWidth={true}
-              onClick={(e) => handleCreateSale(true)}
+              onClick={() => handleCreatePayment(true)}
             >
-              Pagar con ticket
-               (Ctrl + G)
+              Pagar con ticket (Ctrl + G)
             </CustomButton>
           </Col>
 
@@ -219,13 +176,10 @@ const PaymentModal2 = () => {
             <CustomButton
               disabled={handleDisableButton()}
               fullWidth={true}
-              onClick={(e) => handleCreateSale()}
+              onClick={(e) => handleCreatePayment()}
             >
-              Pagar sin ticket
-              
-              (Ctrl + F)
+              Pagar sin ticket (Ctrl + F)
             </CustomButton>
-
           </Col>
         </Row>
       </div>
