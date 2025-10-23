207d740b821009876452862d06e05b1ffd940f13
David <dmg_92@hotmail.com>
2025-10-23 11:32:22 -0600

    week day

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -24,8 +24,8 @@ const Dashboard = () => {
           <Col md={6}>
             <LineChart title={"Test"} taskId={tasks?.task1} labels={["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]}/>
           </Col>
-          <Col md={12}>
-            <LineChart title={"Test"} taskId={tasks?.task2} labels={["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]}/>
+          <Col md={6}>
+            <LineChart title={"Test"} taskId={tasks?.task2} labels={["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]}/>
           </Col>
         </Row>
       </div>
