6648ff6807c91c128f9a911152dc4670de7654a4
David <dmg_92@hotmail.com>
2025-10-23 17:31:40 -0600

    ventas

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -30,11 +30,14 @@ const Dashboard = () => {
           </Col>
 
           <Col md={6}>
-            <LineChart title={"Ventas por dia"} taskId={tasks?.task3} labels={["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]} yText={"Ingresos"} xText={"Dias"}/>
+            <LineChart title={"Ventas por hora"} taskId={tasks?.task3} labels={["0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]} yText={"Ingresos"} xText={"Dias"}/>
           </Col>
 
-          <Col md={4}>
-            <DoughnutChart title={"Metodos de pago"} taskId={tasks?.task4}/>
+          <Col md={3}>
+            <DoughnutChart title={"Metodos de pago (%)"} taskId={tasks?.task4}/>
+          </Col>
+          <Col md={3}>
+            <DoughnutChart title={"Ventas por tienda(%)"} taskId={tasks?.task5}/>
           </Col>
         </Row>
       </div>
