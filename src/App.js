8d734ac7e8e84422ca5ae675b44481c8230ae147
David <dmg_92@hotmail.com>
2026-02-13 18:26:06 -0600

    base app bar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import "./App.css";
 import Login from "./components/login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CustomNavbar from "./components/navbar/Navbar";
 import { useState, useEffect } from "react";
+import MiniDrawer from "./components/navbar/SideBar";
+import { getUserData } from "./components/apis/utils";
+import useKeepAlive from "./components/apis/keepAlive";
+
+// IMPORTA TUS VISTAS
 import SaleCreate from "./components/saleCreate/SaleCreate";
 import SaleList from "./components/saleList/SaleList";
 import TransferList from "./components/transferList/TransferList";
@@ -19,74 +23,65 @@ import LogList from "./components/logList/LogList";
 import TenantPaymentList from "./components/tenantPaymentList/TenantPaymentList";
 import CashFlowList from "./components/cashFlowList/CashFlowList";
 import SellerList from "./components/sellerList/SellerList";
-import { getUserData } from "./components/apis/utils";
 import DepartmentList from "./components/departmentList/DepartmentList";
 import StoreProductImport from "./components/storeProductImport/StoreProductImport";
 import ServiceList from "./components/serviceList/ServiceList";
 import ProductReassign from "./components/productReassign/ProductReassign";
-import useKeepAlive from "./components/apis/keepAlive";
 import AuditDashboard from "./components/auditDashboard/AuditDashboard";
 import Dashboard from "./components/dashboard/Dashboard";
 import DistributionList from "./components/distributionList/DistributionList";
 import RestartRervice from "./components/restartService/RestartService";
 
 function App() {
-
-  useKeepAlive()
+  useKeepAlive();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const user = getUserData();
 
   useEffect(() => {
-    const user = getUserData();
     if (user) {
       setIsLoggedIn(true);
     }
   }, []);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+  const handleLogin = () => setIsLoggedIn(true);
 
   return (
     <Router>
-      {isLoggedIn ? <CustomNavbar /> : ""}
-
       <Routes>
         {isLoggedIn ? (
-          <>
+          <Route element={<MiniDrawer />}>
             <Route path="/tiendas/" element={<StoreList />} />
             <Route path="/ventas/" element={<SaleList />} />
             <Route path="/vender/" element={<SaleCreate />} />
             <Route path="/importar-ventas/" element={<SaleImport />} />
             <Route path="/corte-caja/" element={<CashSummary />} />
             <Route path="/movimientos/" element={<CashFlowList />} />
-            <Route path="/distribuir/" element={<SaleCreate />} />
             <Route path="/distribuciones/" element={<DistributionList />} />
             <Route path="/traspasos/" element={<TransferList />} />
             <Route path="/clientes/" element={<ClientList />} />
             <Route path="/productos/" element={<ProductList />} />
-            <Route path="/importar-productos/" element={<ProductImport />} />
             <Route path="/inventario/" element={<StoreProductList />} />
-            <Route path="/importar-inventario/" element={<StoreProductImport />} />
             <Route path="/marcas/" element={<BrandList />} />
             <Route path="/departamentos/" element={<DepartmentList />} />
-            <Route path="/reasignacion/" element={<ProductReassign />} />
             <Route path="/logs/" element={<LogList />} />
             <Route path="/pagos/" element={<TenantPaymentList />} />
             <Route path="/vendedores/" element={<SellerList />} />
             <Route path="/servicios/" element={<ServiceList />} />
-            <Route path="/tablero-auditoria/" element={<AuditDashboard />} />
             <Route path="/tablero/" element={<Dashboard />} />
-            <Route path="/sincronizar/" element={<RestartRervice />} />
-            {user.store_id ? (<Route path="/*" element={<SaleCreate />} />): (<Route path="/*" element={<StoreList />} />)}
-          </>
+  
+            {user?.store_id ? (
+              <Route path="*" element={<SaleCreate />} />
+            ) : (
+              <Route path="*" element={<StoreList />} />
+            )}
+          </Route>
         ) : (
-          <Route path="/" element={<Login onLogin={handleLogin} />} />
+          <Route path="*" element={<Login onLogin={handleLogin} />} />
         )}
       </Routes>
     </Router>
   );
+  
 }
 
 export default App;
---
+++ src/components/navbar/AppBar.jsx
@@ -0,0 +1,235 @@
+
+
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { alpha, Badge, BadgeMark, Box, InputBase, Menu, MenuItem, styled } from '@mui/material';
+import React from 'react';
+import { AccountCircle } from '@mui/icons-material';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { getUserData } from '../apis/utils';
+import { useNavigate } from 'react-router-dom';
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+
+export default function PrimarySearchAppBar() {
+const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+
+  const user = getUserData();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    window.location.href = "/";
+  };
+
+  const handleBack = () => {
+    const updatedUser = { ...user, store_type: "", store_name: "", store_id: "" };
+    localStorage.setItem("user", JSON.stringify(updatedUser));
+    navigate("/tiendas/");
+    window.location.reload();
+  };
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Negocio: {user.tenant_name}</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Usuario: {user.full_name}</MenuItem>
+
+      {user.role === "owner" && user.store_id && (
+            <MenuItem onClick={handleBack}>Regresar</MenuItem>
+          )}
+
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        Negocio: {user.tenant_name}
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            SmartVenta
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+
+          <IconButton
+              size="large"
+              aria-label="Logout"
+              color="inherit"
+            >
+                <LogoutIcon onClick={handleLogout}/>
+            </IconButton>
+          
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
