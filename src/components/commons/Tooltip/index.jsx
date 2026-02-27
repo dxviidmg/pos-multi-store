832ad7b2af45e0f6f85a7357ce9fecb812ea4a4c
David <dmg_92@hotmail.com>
2026-02-27 03:15:49 -0600

    tooltip

--- src/components/commons/Tooltip/index.jsx
+++ src/components/commons/Tooltip/index.jsx
@@ -1,19 +1,13 @@
 import { memo } from "react";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Tooltip } from "@mui/material";
 
 export const CustomTooltip = memo(({ children, text, position }) => {
-  const renderTooltip = (props) => (
-    <Tooltip id="tooltip-hover" {...props} className="custom-tooltip-primary">
-      {text}
-    </Tooltip>
-  );
-
   return (
-    <div>
-      <OverlayTrigger placement={position || "right"} overlay={renderTooltip}>
-        <div>{children}</div>
-      </OverlayTrigger>
-    </div>
+    <Tooltip title={text} placement={position || "right"} arrow>
+      <span style={{ display: 'inline-block' }}>
+        {children}
+      </span>
+    </Tooltip>
   );
 });
 
