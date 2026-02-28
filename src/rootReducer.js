edba65d2de0ebb917cc9bb574607bbd71e1d46ef
David <dmg_92@hotmail.com>
2026-02-27 19:45:16 -0600

    refactor: reemplazar modales Redux con hook useModal
    - Migrados 12 modales a hook useModal
    - Eliminados 11 reducers de modales (528 líneas)
    - Redux ahora solo maneja estado del carrito
    - Reducción del 92% en complejidad de Redux

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,32 +1,10 @@
 import { combineReducers } from "redux";
 import cartReducer from "./redux/cart/cartReducer";
 import multiCartReducer from "./redux/cart/multiCartReducer";
-import StockModalReducer from "./redux/stockModal/StockModalReducer";
-import PaymentModalReducer from "./redux/paymentModal/PaymentModalReducer";
-import BrandModalReducer from "./redux/brandModal/BrandModalReducer";
-import ProductModalReducer from "./redux/productModal/ProductModalReducer";
-import ClientModalReducer from "./redux/clientModal/ClientModalReducer";
-import SaleModalReducer from "./redux/saleModal/SaleModalReducer";
-import LogsModalReducer from "./redux/logsModal/LogsModalReducer";
-import CashFlowModalReducer from "./redux/cashFlowModal/CashFlowModalReducer";
-import SellerModalReducer from "./redux/sellerModal/SellerModalReducer";
-import DepartmentModalReducer from "./redux/departmentModal/DepartmentModalReducer";
-import PaymentModal2Reducer from "./redux/paymentReservationModal/PaymentReservationModalReducer";
 
 const rootReducer = combineReducers({
   cartReducer,
   multiCartReducer,
-  StockModalReducer,
-  PaymentModalReducer,
-  BrandModalReducer,
-  ProductModalReducer,
-  ClientModalReducer,
-  SaleModalReducer,
-  LogsModalReducer,
-  CashFlowModalReducer,
-  SellerModalReducer,
-  DepartmentModalReducer,
-  PaymentModal2Reducer
 });
 
 export default rootReducer;
