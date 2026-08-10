f263752d1b6a77567589a0c98b2eb626784ae3b0
David <davidmejia.05@hotmail.com>
2026-08-10 12:55:17 -0600

    Mover Registration de tenants/ a tenant/ para consistencia de nomenclatura

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const PriceLogsList = lazyRetry(() => import("./components/products/PriceLogsLis
 const DistributionList = lazyRetry(() => import("./components/inventory/DistributionList/DistributionList"));
 const RestartService = lazyRetry(() => import("./components/admin/RestartService/RestartService"));
 const Profile = lazyRetry(() => import("./components/admin/Profile/Profile"));
-const Registration = lazyRetry(() => import("./components/tenants/Registration/Registration"));
+const Registration = lazyRetry(() => import("./components/tenant/Registration/Registration"));
 
 function App({ toggleTheme, themeMode }) {
   const { user } = useUser();
